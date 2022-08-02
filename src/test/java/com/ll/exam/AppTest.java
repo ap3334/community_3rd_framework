1ecb2236a40c7865d3623a209b9110aea28e7678
ap3334 <chwil2989@naver.com>
2022-08-02 10:26:48 +0900

    Article Controller 싱글톤으로 작동

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -12,6 +13,23 @@ public class AppTest {
         int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    public void ioc_articleController() {
+
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+
+    @Test
+    public void ioc__articleController__싱글톤() {
+
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController1).isEqualTo(articleController2);
     }
 
 }
