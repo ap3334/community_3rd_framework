2b287eb6fbc8c8caf249abe2b8d124c1824fb847
ap3334 <chwil2989@naver.com>
2022-08-02 13:20:36 +0900

    Ut.cls.newObj 로 객체 생성

--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -1,6 +1,29 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class Ut {
+    public static class cls {
+
+        public static Object newObj(Class<?> cls, Object defaultValue) {
+
+            try {
+                return cls.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException e) {
+                return defaultValue;
+            } catch (IllegalAccessException e) {
+                return defaultValue;
+            } catch (InvocationTargetException e) {
+                return defaultValue;
+            } catch (NoSuchMethodException e) {
+                return defaultValue;
+            }
+
+        }
+    }
+
     public static class Str {
 
         public static String deCapitalize(String string) {
