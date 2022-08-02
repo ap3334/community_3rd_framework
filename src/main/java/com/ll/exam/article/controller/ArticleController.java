9e95f8b539dd4c640ed28ad3df54c8e004c9ac3d
ap3334 <chwil2989@naver.com>
2022-08-02 10:37:59 +0900

    어노테이션 추가

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -1,4 +1,14 @@
 package com.ll.exam.article.controller;
 
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
+
+@Controller // ArticleController가 Controller임을 명시
 public class ArticleController {
+
+    @GetMapping("/usr/article/list")
+    // GET /usr/article/list로 요청이 왔을 때 작동하는 함수
+    public void showList() {
+
+    }
 }
